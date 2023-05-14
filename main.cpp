b4314c0704a90dafde59886dcb298aa806634cf9
Антон Брыляков <yoll@rambler.ru>
2023-05-14 17:21:05 +0700

    Начальный коммит

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QCoreApplication>
+#include <QTcpServer>
+#include <QSqlDatabase>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QTcpServer serv();
+
+    QSqlDatabase db();
+
+    return a.exec();
+}
